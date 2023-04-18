73c84e0e5f30d271ea81db2a10570d27806c63ab
xkvasn14 <61392422+xkvasn14@users.noreply.github.com>
2023-04-18 18:37:57 +0200

    fix Game

--- src/main/java/window/Game.java
+++ src/main/java/window/Game.java
@@ -3,7 +3,7 @@ package window;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class game extends Application {
+public class Game extends Application {
 
     public static void main(String[] args) {
         launch(args);
@@ -11,6 +11,6 @@ public class game extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // this is game
+        // this is Game
     }
 }
